a2b5dfe0e64238bca5f4d2b6f70e9898c62a4a2c
SyedAmer-9 <syedamersaph@gmail.com>
2025-10-27 05:41:48 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup,find_packages
-
-setup(
-    name = 'src',
-    version = '0.0.1',
-    author = 'Amer',
-    author_email='cosmiclover2003@gmail.com',
-    packages=find_packages()
-)
