ddac9bd78b1ed1a9a9220e3efa4762439e319496
amxr <cosmiclover2003@gmail.com>
2025-10-29 14:32:30 +0530

    added logging and exception

---
+++ demo.py
@@ -0,0 +1,31 @@
+# from src.logger import logging
+
+# logging.debug("This is a debug message.")
+# logging.info("This is an info message")
+# logging.warning("This is a warning message")
+# logging.error("This is an error message")
+# logging.critical("This is a critical message.")
+
+
+import sys
+from src.exception import MyException
+from src.logger import logging # This just ensures logging is configured
+
+def test_function():
+    try:
+        logging.info("Starting test function...")
+        x = 1 / 0  # This will cause an error
+    except Exception as e:
+        # This is where the magic happens
+        # 1. 'e' is the ZeroDivisionError
+        # 2. 'sys' has access to the traceback
+        # 3. We raise our OWN exception
+        raise MyException(e, sys)
+
+# --- Main execution ---
+if __name__ == "__main__":
+    try:
+        test_function()
+    except MyException as e:
+        # The program will stop, and __str__ is called
+        print(e)
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='src',
+    version='0.0.1',
+    author='amer',
+    author_email='amer.workspace09@gmail.com',
+    packages=find_packages()
+)
