4afe664fd203bc21dd47ea1136c57963af41c942
amxr <cosmiclover2003@gmail.com>
2025-10-31 02:44:32 +0530

    data ingestion added

--- demo.py
+++ demo.py
@@ -7,25 +7,30 @@
 # logging.critical("This is a critical message.")
 
 
-import sys
-from src.exception import MyException
-from src.logger import logging # This just ensures logging is configured
+# import sys
+# from src.exception import MyException
+# from src.logger import logging # This just ensures logging is configured
 
-def test_function():
-    try:
-        logging.info("Starting test function...")
-        x = 1 / 0  # This will cause an error
-    except Exception as e:
-        # This is where the magic happens
-        # 1. 'e' is the ZeroDivisionError
-        # 2. 'sys' has access to the traceback
-        # 3. We raise our OWN exception
-        raise MyException(e, sys)
+# def test_function():
+#     try:
+#         logging.info("Starting test function...")
+#         x = 1 / 0  # This will cause an error
+#     except Exception as e:
+#         # This is where the magic happens
+#         # 1. 'e' is the ZeroDivisionError
+#         # 2. 'sys' has access to the traceback
+#         # 3. We raise our OWN exception
+#         raise MyException(e, sys)
 
-# --- Main execution ---
-if __name__ == "__main__":
-    try:
-        test_function()
-    except MyException as e:
-        # The program will stop, and __str__ is called
-        print(e)
+# # --- Main execution ---
+# if __name__ == "__main__":
+#     try:
+#         test_function()
+#     except MyException as e:
+#         # The program will stop, and __str__ is called
+#         print(e)
+
+from src.pipline.training_pipeline import TrainPipeline
+
+pipline=TrainPipeline()
+pipline.run_pipeline()
